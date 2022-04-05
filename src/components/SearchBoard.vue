a93f65fa58677ea40cdc043de4187df072f8f843
马钿雨 <2020010881@secoder.net>
2022-04-05 16:19:56 +0800

    实现前后端通信 待测试 #4

--- src/components/SearchBoard.vue
+++ src/components/SearchBoard.vue
@@ -1,38 +1,52 @@
 <template>
   <div id="search-board">
-    <SearchBox :query="query" :search="search"/>
-    <CardBox :cardContent="cardContent"/>
+    <SearchBox :query="query" :search="search" />
+    <CardBox :cardContent="cardContent" />
   </div>
 </template>
 
 <script>
-import SearchBox from "./SearchBox"
-import CardBox from "./CardBox"
+import request_json from "../utils/communication";
+import SearchBox from "./SearchBox";
+import CardBox from "./CardBox";
 export default {
-  name: 'SearchBoard',
+  name: "SearchBoard",
   components: {
     SearchBox,
     CardBox,
   },
-  data(){
+  data() {
     return {
-        query: "",
-        cardContent: "666",
+      query: "",
+      cardContent: "666",
+    };
+  },
+  methods: {
+    //wait to test
+    search(query) {
+      this.query = query;
+      var query_json = {
+        "query": this.query
+      }
+      request_json.POST(this.query_succeed, this.get_result, query_json);
+    },
+    query_succeed(bool) {
+      if (bool) {
+        alert("查询成功！");
+      } else {
+        alert("查询失败！");
+      }
+    },
+    get_result(res) {
+      this.cardContent = res;
     }
   },
-  methods:{
-      search(query) {
-          this.query = query;
-          this.cardContent = query;
-      }
-  }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #search-board {
-    margin-top: 20px;
+  margin-top: 20px;
 }
-
 </style>
