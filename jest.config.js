127d2c7ca058976dd9f4bbf3b314bf95fddba73c
容逸朗 <2020010869@secoder.net>
2022-05-07 11:38:01 +0800

    add axios branch test #13

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   testResultsProcessor: 'jest-sonar-reporter',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
-    '!src/main.js',
     "!<rootDir>/node_modules/",
     "!<rootDir>/path/to/dir/",
   ],
--- tests/unit/CarNavTab.spec.js
+++ tests/unit/CarNavTab.spec.js
@@ -28,3 +28,118 @@ describe('CarNavTab.vue', () => {
   })
 
 })
+
+describe('CarNavTab.vue with > 5 items and > 4 tags', () => {
+  const wrapper = shallowMount(CarNavTab, {
+    propsData: {
+      itemKey: 0,
+      content: [{
+        tag: "hot",
+        series_list: {
+          item: [{
+            series_name: "sr1",
+            price: "1",
+          }, {
+            series_name: "sr2",
+            price: "2",
+          }, {
+            series_name: "sr3",
+            price: "3",
+          }, {
+            series_name: "sr4",
+            price: "4",
+          }, {
+            series_name: "sr5",
+            price: "5",
+          }]
+        }
+      }, {
+        tag: "hot",
+        series_list: {
+          item: [{
+            series_name: "sr1",
+            price: "1",
+          }, {
+            series_name: "sr2",
+            price: "2",
+          }, {
+            series_name: "sr3",
+            price: "3",
+          }, {
+            series_name: "sr4",
+            price: "4",
+          }, {
+            series_name: "sr5",
+            price: "5",
+          }]
+        }
+      }, {
+        tag: "hot",
+        series_list: {
+          item: [{
+            series_name: "sr1",
+            price: "1",
+          }, {
+            series_name: "sr2",
+            price: "2",
+          }, {
+            series_name: "sr3",
+            price: "3",
+          }, {
+            series_name: "sr4",
+            price: "4",
+          }, {
+            series_name: "sr5",
+            price: "5",
+          }]
+        }
+      }, {
+        tag: "hot",
+        series_list: {
+          item: [{
+            series_name: "sr1",
+            price: "1",
+          }, {
+            series_name: "sr2",
+            price: "2",
+          }, {
+            series_name: "sr3",
+            price: "3",
+          }, {
+            series_name: "sr4",
+            price: "4",
+          }, {
+            series_name: "sr5",
+            price: "5",
+          }]
+        }
+      }, {
+        tag: "hot",
+        series_list: {
+          item: [{
+            series_name: "sr1",
+            price: "1",
+          }, {
+            series_name: "sr2",
+            price: "2",
+          }, {
+            series_name: "sr3",
+            price: "3",
+          }, {
+            series_name: "sr4",
+            price: "4",
+          }, {
+            series_name: "sr5",
+            price: "5",
+          }]
+        }
+      }]
+    }
+  })
+
+  it('check context', () => {
+    const title = wrapper.find('#carnavtab_0')
+    expect(title.text()).toBe('sr1, 1')
+  })
+
+})
